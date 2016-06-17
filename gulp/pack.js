53b9ed7ff7d7ad1fd58d86a7d2b3945611318d3d
stevie <whotopia@gmail.com>
2016-06-17 23:37:40 +0200

    message.

--- gulp/pack.js
+++ gulp/pack.js
@@ -3,15 +3,15 @@ var zip = require('gulp-zip');
 var shell = require('gulp-shell');
 var argv = require('yargs').argv;
 gulp.task('pack', ['build'], function() {
-    return gulp.src('dist/**/*').pipe(zip('spectrum.zip')).pipe(gulp.dest('target'));
+    return gulp.src('dist/**/*').pipe(zip('hivpopdata.zip')).pipe(gulp.dest('target'));
 });
 gulp.task('deploy', ['pack'], function() {
     var username = argv.username || 'admin';
     var password = argv.password || 'district';
     var url = argv.url || 'http://localhost:8080';
 
-    return gulp.src('target/spectrum.zip').pipe(shell(['curl -X DELETE -u ' + username + ':' + password + ' ' + url +
-        '/api/apps/spectrum', 'curl -X POST -u' + username + ':' + password + ' -F file=@<%= file.path %> ' + url +
+    return gulp.src('target/hivpopdata.zip').pipe(shell(['curl -X DELETE -u ' + username + ':' + password + ' ' + url +
+        '/api/apps/hivpopdata', 'curl -X POST -u' + username + ':' + password + ' -F file=@<%= file.path %> ' + url +
         '/api/apps'
     ]));
 });
